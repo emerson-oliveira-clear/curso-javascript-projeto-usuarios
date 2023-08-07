2b0157af40aace3f367cfd841594aca536695955
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-08-07 10:15:48 -0300

    fist commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let name = document.querySelector("#exampleInputName");
+let gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+let birth = document.querySelector("#exampleInputBirth");
+let country = document.querySelector("#exampleInputCountry");
+let email = document.querySelector("#exampleInputEmail");
+let password = document.querySelector("#exampleInputPassword");
+let photo = document.querySelector("#exampleInputFile");
+let admin = document.querySelector("#exampleInputAdmin");
+
+let fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach(function(field, index){
+
+    console.log(field.name);
+
+});
