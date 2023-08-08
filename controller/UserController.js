a7277e186e2c7002a1f80a0aac28c0b76f837573
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-08-08 09:31:06 -0300

    form validation

--- controller/UserController.js
+++ controller/UserController.js
@@ -93,8 +93,16 @@ class UserController {
     getValues() {
 
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach(function (field, index) {
+
+            if(['name','email','password'].indexOf(field.name) > -1 && !field.value){
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+
+            }
 
             if (field.name == "gender") {
 
@@ -116,6 +124,12 @@ class UserController {
             }
 
         });
+
+        if (!isValid){
+            
+            return false;
+
+        }
 
         return new User(
             user.name,
